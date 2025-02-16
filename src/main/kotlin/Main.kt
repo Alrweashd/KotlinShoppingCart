4b9d4a59e9bfb2a423a0beb9ccb66bb492435ab7
alrweashd <trkyabler@gmail.com>
2025-02-16 17:09:09 +0300

    Logical for addCart

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,13 @@ package shoppingCart
  *   5 -> { "name"="Wireless Mouse", "price"=8.500, "stock"=15 },
  */
 fun createStoreInventory(): MutableMap<Int, MutableMap<String, Any>> {
-    TODO("Implement createStoreInventory()")
+    return mutableMapOf(
+        1 to mutableMapOf("name" to "Laptop", "price" to 350.000, "stock" to 10),
+        2 to mutableMapOf("name" to "Smart TV", "price" to 200.000, "stock" to 5),
+        3 to mutableMapOf("name" to "Headphones", "price" to 50.000, "stock" to 20),
+        4 to mutableMapOf("name" to "Gaming Console", "price" to 150.000, "stock" to 8),
+        5 to mutableMapOf("name" to "Wireless Mouse", "price" to 8.500, "stock" to 15),
+    )
 }
 
 /**
@@ -19,13 +25,45 @@ fun createStoreInventory(): MutableMap<Int, MutableMap<String, Any>> {
  * - If valid, reduce the stock and increase the cart quantity for this product.
  * - Return true if successful, false otherwise.
  */
+
+
+// id , qu
 fun addToCart(
     storeInventory: MutableMap<Int, MutableMap<String, Any>>,
     cart: MutableMap<Int, Int>,
     productId: Int,
     quantity: Int
 ): Boolean {
-    TODO("Implement addToCart()")
+    //We first check if the product x with the provided id exists in the store.
+    val product = storeInventory[productId];
+    println(product);
+    if (product==null){
+        println("Product not found!!")
+        return false;
+    }
+    // if not enough stock
+    println("Product is $product")
+    val currentStock = product["stock"] as Int
+    println("productStock $currentStock")
+    if(quantity > currentStock){
+        println("current stock is [$currentStock] which is less than the requested quantity [$quantity] !!")
+        return false
+    }
+
+    //Reduce stock
+    product["stock"] = currentStock - quantity;
+    println("Stock of product ID [$productId] for the product name [${product["name"]}] updated to [${product["stock"]}].")
+    //Updating the cart by adding the product to it. if exist in the cart make the value zero
+    val currentQuantity = cart[productId] ?: 0;
+    println("currentQuantity $currentQuantity")
+    cart[productId] = currentQuantity + quantity
+    println("Product ID $productId quantity in cart updated to ${cart[productId]}.")
+    println("[$quantity] ${product["name"]} have been successfully added to your cart! with the product ID $productId ")
+    return true
+
+
+//       if(storeInventory.keys.filter {  })
+
 }
 
 /**
@@ -51,7 +89,9 @@ fun calculateTotal(
     storeInventory: MutableMap<Int, MutableMap<String, Any>>,
     cart: MutableMap<Int, Int>
 ): Double {
-    TODO("Implement calculateTotal()")
+
+//    storeInventory.keys.find { }
+    TODO("Implement filterProductsByName()")
 }
 
 /**
@@ -71,7 +111,13 @@ fun filterProductsByName(
  * Run the JUnit tests to see if you've implemented each function correctly.
  */
 fun main() {
+    val storeInventory = createStoreInventory()
+    val cart = mutableMapOf<Int,Int>()
     println("Welcome to the Kotlin Store project!")
     println("All functions currently throw NotImplementedError.")
     println("Open each function and replace TODO(...) with your implementation.")
+
+    println(createStoreInventory())
+    addToCart(storeInventory,cart,1,3)
+    addToCart(storeInventory,cart,1,10)
 }
